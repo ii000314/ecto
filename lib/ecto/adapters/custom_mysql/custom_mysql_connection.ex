efc8b90c4345d0e652aea984b5d3012ef9818c7e
Valentine Nikonovich <v_nikonovich@wargaming.net>
2017-08-22 15:46:05 +0300

    Fix return value of execute

--- lib/ecto/adapters/custom_mysql/custom_mysql_connection.ex
+++ lib/ecto/adapters/custom_mysql/custom_mysql_connection.ex
@@ -52,10 +52,7 @@ if Code.ensure_loaded?(Mariaex) do
       # end
       statement = inject_params_to_statement(sql, params)
       query = %Mariaex.Query{type: :text, statement: statement, ref: make_ref(), num_params: 0}
-      case DBConnection.execute(conn, query, [], opts) do
-        {:ok, result} -> {:ok, result}
-        {:error, reason} -> {:error, reason}
-      end
+      DBConnection.execute(conn, query, [], opts)
     end
 
     def execute(conn, %{} = query, params, opts) do
